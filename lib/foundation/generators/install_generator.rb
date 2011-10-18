3ea034e3efe5453772eff2fe33baabaeeeaf96ea
Mark Hayes <mark@zurb.com>
2011-10-18 11:04:23 -0700

    rails g foundation:install should only append to application sprockets files

--- lib/foundation/generators/install_generator.rb
+++ lib/foundation/generators/install_generator.rb
@@ -5,9 +5,9 @@ module Foundation
     class InstallGenerator < Rails::Generators::Base
       source_root File.join(File.dirname(__FILE__), 'templates')
       
-      def replace_assets
-        copy_file 'application.js', "app/assets/javascripts/application.js"
-        copy_file 'application.css', "app/assets/stylesheets/application.css"
+      def add_assets
+        insert_into_file "app/assets/javascripts/application.js", "//= require foundation\n", :after => "jquery_ujs\n"
+        insert_into_file "app/assets/stylesheets/application.css", " *= require foundation\n", :after => "require_self\n"
       end
     end
   end
