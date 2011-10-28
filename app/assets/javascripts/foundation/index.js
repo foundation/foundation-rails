bde6bbe9274227bcd72d095f89072a69d03be775
Mark Hayes <mark@zurb.com>
2011-10-28 10:49:42 -0700

    updated to foundation 2.0.2.  also all assets have been moved from app/assets to vendor/assets.

--- app/assets/javascripts/foundation/index.js
+++
@@ -1,6 +0,0 @@
-//=require ./jquery.reveal.js
-//=require ./jquery.orbit-1.3.0.js
-//=require ./forms.jquery.js
-//=require ./jquery.customforms.js
-//=require ./jquery.placeholder.min.js
-//=require ./app.js
