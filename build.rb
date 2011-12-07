015269641aae4f4cf0430c07d1044491cec103c7
Mark Hayes <mark@zurb.com>
2011-12-06 18:30:03 -0800

    updated build script to place all foundation files into a manifest so users will have the option to selectively include stylesheets in their app

--- build.rb
+++ build.rb
@@ -13,25 +13,33 @@ ASSET_PATH = Pathname.new(File.dirname(__FILE__)).join("vendor", "assets")
 FileUtils.remove_dir ASSET_PATH, :force => true
 
 # Recreate vendor/assets
-FileUtils.mkdir_p ASSET_PATH.join("stylesheets")
-FileUtils.mkdir_p ASSET_PATH.join("javascripts")
+FileUtils.mkdir_p ASSET_PATH.join("stylesheets", "foundation")
+FileUtils.mkdir_p ASSET_PATH.join("javascripts", "foundation")
 FileUtils.mkdir_p ASSET_PATH.join("images", "foundation")
 
 # Create SCSS file, so we can usage image-url() helper
-File.open(ASSET_PATH.join("stylesheets", "foundation.scss"), "w") do |file|
+File.open(ASSET_PATH.join("stylesheets", "foundation", "index.css.scss"), "w") do |file|
+  file << "/*\n"
   FOUNDATION_CSS.each do |filename|
+    file << "*= require 'foundation/#{filename.gsub('.css','')}'\n"
     css_file = File.open(FOUNDATION_REPO_PATH.join("stylesheets", filename))
-    file << css_file.read.gsub(/url\('?\.\.\/images\/([^\)']+)'?\)/, 'image-url("foundation/\1")')
-    file << "\n\n"
+    File.open(ASSET_PATH.join("stylesheets", "foundation", "#{filename}.scss"), "w") do |include_file|
+      include_file << css_file.read.gsub(/url\('?\.\.\/images\/([^\)']+)'?\)/, 'image-url("foundation/\1")')
+      include_file << "\n\n"
+    end
   end
+  file << "*/"
 end
 
 # Create JS file
-File.open(ASSET_PATH.join("javascripts", "foundation.js"), "w") do |file|
+File.open(ASSET_PATH.join("javascripts", "foundation", "index.js"), "w") do |file|
   FOUNDATION_JS.each do |filename|    
+    file << "//= require 'foundation/#{File.basename(filename.gsub('.js',''))}'\n"
     js_file = File.open(FOUNDATION_REPO_PATH.join("javascripts", filename))
-    file << js_file.read
-    file << "\n\n"
+    File.open(ASSET_PATH.join("javascripts", "foundation", filename), "w") do |include_file|
+      include_file << js_file.read
+      include_file << "\n\n"
+    end
   end
 end
 
