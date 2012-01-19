d868d9d2ee35c4b96e73f9c50422ba0f632824e6
David N. Welton <davidw@dedasys.com>
2012-01-19 10:39:38 +0100

    Revert "Updated files from Zurb Foundation."
    This reverts commit 0464e7297ae622922980d69e2baf5af7ffd85c4d.

--- vendor/assets/javascripts/foundation/jquery.customforms.js
+++ vendor/assets/javascripts/foundation/jquery.customforms.js
@@ -23,41 +23,33 @@ jQuery(document).ready(function ($) {
   }
   appendCustomMarkup('checkbox');
   appendCustomMarkup('radio');
-
-  function appendCustomSelect(sel) {
-    var $this = $(sel),
+  
+  $('form.custom select').each(function () {
+    var $this = $(this),
         $customSelect = $this.next('div.custom.dropdown'),
         $options = $this.find('option'),
         maxWidth = 0,
         $li;
-
-    if ($customSelect.length === 0) {
+    
+    if ($customSelect.length === 0) {      
       $customSelect = $('<div class="custom dropdown"><a href="#" class="selector"></a><ul></ul></div>"');
       $options.each(function () {
         $li = $('<li>' + $(this).html() + '</li>');
         $customSelect.find('ul').append($li);
       });
       $customSelect.prepend('<a href="#" class="current">' + $options.first().html() + '</a>');
-
+      
       $this.after($customSelect);
       $this.hide();
-      
-    } else {
-      // refresh the ul with options from the select in case the supplied markup doesn't match
-      $customSelect.find('ul').html('');
-      $options.each(function () {
-        $li = $('<li>' + $(this).html() + '</li>');
-        $customSelect.find('ul').append($li);
-      });
     }
-
+    
     $options.each(function (index) {
       if (this.selected) {
         $customSelect.find('li').eq(index).addClass('selected');
         $customSelect.find('.current').html($(this).html());
       }
     });
-
+    
     $customSelect.find('li').each(function () {
       $customSelect.addClass('open');
       if ($(this).outerWidth() > maxWidth) {
@@ -67,50 +59,10 @@ jQuery(document).ready(function ($) {
     });
     $customSelect.css('width', maxWidth + 18 + 'px');
     $customSelect.find('ul').css('width', maxWidth + 16 + 'px');
-
-  }
-
-  $('form.custom select').each(function () {
-    appendCustomSelect(this);
   });
-  
 });
 
 (function ($) {
-  
-  function refreshCustomSelect($select) {
-    var maxWidth = 0,
-        $customSelect = $select.next();
-    $options = $select.find('option');
-    $customSelect.find('ul').html('');
-    
-    $options.each(function () {
-      $li = $('<li>' + $(this).html() + '</li>');
-      $customSelect.find('ul').append($li);
-    });
-    
-    // re-populate
-    $options.each(function (index) {
-      if (this.selected) {
-        $customSelect.find('li').eq(index).addClass('selected');
-        $customSelect.find('.current').html($(this).html());
-      }
-    });
-    
-    // fix width
-    $customSelect.removeAttr('style')
-      .find('ul').removeAttr('style');
-    $customSelect.find('li').each(function () {
-      $customSelect.addClass('open');
-      if ($(this).outerWidth() > maxWidth) {
-        maxWidth = $(this).outerWidth();
-      }
-      $customSelect.removeClass('open');
-    });
-    $customSelect.css('width', maxWidth + 18 + 'px');
-    $customSelect.find('ul').css('width', maxWidth + 16 + 'px');
-    
-  }
   
   function toggleCheckbox($element) {
     var $input = $element.prev(),
@@ -135,25 +87,21 @@ jQuery(document).ready(function ($) {
     $input.trigger('change');
   }
   
-  $('form.custom span.custom.checkbox').live('click', function (event) {
+  $(document).on('click', 'form.custom span.custom.checkbox', function (event) {
     event.preventDefault();
     event.stopPropagation();
     
     toggleCheckbox($(this));
   });
   
-  $('form.custom span.custom.radio').live('click', function (event) {
+  $(document).on('click', 'form.custom span.custom.radio', function (event) {
     event.preventDefault();
     event.stopPropagation();
     
     toggleRadio($(this));
   });
   
-  $('form.custom select').live('change', function (event) {
-    refreshCustomSelect($(this));
-  });
-  
-  $('form.custom label').live('click', function (event) {
+  $(document).on('click', 'form.custom label', function (event) {
     var $associatedElement = $('#' + $(this).attr('for')),
         $customCheckbox,
         $customRadio;
@@ -170,7 +118,7 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  $('form.custom div.custom.dropdown a.current, form.custom div.custom.dropdown a.selector').live('click', function (event) {
+  $(document).on('click', 'form.custom div.custom.dropdown a.current, form.custom div.custom.dropdown a.selector', function (event) {
     var $this = $(this),
         $dropdown = $this.closest('div.custom.dropdown');
     
@@ -187,7 +135,7 @@ jQuery(document).ready(function ($) {
     }
   });
   
-  $('form.custom div.custom.dropdown li').live('click', function (event) {
+  $(document).on('click', 'form.custom div.custom.dropdown li', function (event) {
     var $this = $(this),
         $customDropdown = $this.closest('div.custom.dropdown'),
         $select = $customDropdown.prev(),
@@ -218,3 +166,5 @@ jQuery(document).ready(function ($) {
     $select.trigger('change');
   });
 })(jQuery);
+
+
