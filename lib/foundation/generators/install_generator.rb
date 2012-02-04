d05f29ab749e4b2bf61edfb021edff41e8559882
Gonzalo Rodríguez-Baltanás Díaz <siotopo@gmail.com>
2012-02-04 14:04:28 +0100

    Detect application.{extension} when generating assets

--- lib/foundation/generators/install_generator.rb
+++ lib/foundation/generators/install_generator.rb
@@ -5,9 +5,20 @@ module Foundation
     class InstallGenerator < Rails::Generators::Base
       source_root File.join(File.dirname(__FILE__), 'templates')
       
-      def add_assets
-        insert_into_file "app/assets/javascripts/application.js", "//= require foundation\n", :after => "jquery_ujs\n"
-        insert_into_file "app/assets/stylesheets/application.css", " *= require foundation\n", :after => "require_self\n"
+      def add_assets                  
+        insert_into_file "app/assets/javascripts/application#{detect_js_format}", "//= require foundation\n", :after => "jquery_ujs\n"
+        insert_into_file "app/assets/stylesheets/application#{detect_css_format}", " *= require foundation\n", :after => "require_self\n"              
+      end
+      
+      def detect_js_format
+        return '.js.coffee' if File.exist?('app/assets/javascripts/application.js.coffee')
+        return '.js' if File.exist?('app/assets/javascripts/application.js')
+      end
+      
+      def detect_css_format
+        return '.css' if File.exist?('app/assets/stylesheets/application.css')
+        return '.css.sass' if File.exist?('app/assets/stylesheets/application.css.sass')
+        return '.css.scss' if File.exist?('app/assets/stylesheets/application.css.scss')
       end
     end
   end
