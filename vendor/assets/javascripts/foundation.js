foundation/foundation-rails

commit acd6aa298354443e8b2f1c2fd45e8530f8f00d40

ruby version number change for deploy

vendor/assets/javascripts/foundation.js
@@ -1,4 +1,3 @@
-//= require foundation/foundation
 //= require foundation/foundation.abide
 //= require foundation/foundation.accordion
 //= require foundation/foundation.alert
@@ -7,6 +6,7 @@
 //= require foundation/foundation.equalizer
 //= require foundation/foundation.interchange
 //= require foundation/foundation.joyride
+//= require foundation/foundation
 //= require foundation/foundation.magellan
 //= require foundation/foundation.offcanvas
 //= require foundation/foundation.orbit

.ruby-version
@@ -1,1 +1,1 @@
-2.2.1
+2.2.0

commit 88722bfcbca67bd69f82b6f7f81ae1f7ea8e22eb

load foundation core first. fixes bug introduced in acd6aa29

vendor/assets/javascripts/foundation.js
@@ -1,3 +1,4 @@
+//= require foundation/foundation
 //= require foundation/foundation.abide
 //= require foundation/foundation.accordion
 //= require foundation/foundation.alert
@@ -6,7 +7,6 @@
 //= require foundation/foundation.equalizer
 //= require foundation/foundation.interchange
 //= require foundation/foundation.joyride
-//= require foundation/foundation
 //= require foundation/foundation.magellan
 //= require foundation/foundation.offcanvas
 //= require foundation/foundation.orbit